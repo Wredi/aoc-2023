76e2a22506c0eafbc0532db43d33f313c9bfcf34
Wredi <piotr.skora25@gmail.com>
2023-12-02 00:35:16 +0100

    first day completed

---
+++ day_01/day_01.hs
@@ -0,0 +1,32 @@
+import Data.Char
+import Data.List
+import Data.Bifunctor
+import Control.Monad
+
+initDigitMap = [ ("one", '1')
+            , ("two", '2') 
+            , ("three", '3') 
+            , ("four", '4') 
+            , ("five", '5') 
+            , ("six", '6') 
+            , ("seven", '7') 
+            , ("eight", '8')
+            , ("nine", '9') ]
+
+digitFromPrefix :: [(String, Char)] -> String -> Maybe Char
+digitFromPrefix digitMap input@(c:_)
+    | isDigit c = Just c 
+    | otherwise = foldl (\acc (str, dig) -> if str `isPrefixOf` input then Just dig else acc) Nothing digitMap 
+
+firstDigit :: [(String, Char)] -> String -> Maybe Char
+firstDigit digitMap all@(_:cs)
+    | currDigit == Nothing = firstDigit digitMap cs 
+    | otherwise = currDigit
+    where currDigit = digitFromPrefix digitMap all
+
+parseLine :: String -> Maybe Int
+parseLine line = read <$> sequence [leftNum, rightNum]
+    where leftNum = firstDigit initDigitMap line 
+          rightNum = firstDigit (map (first reverse) initDigitMap) (reverse line)
+
+main = readFile "input" >>= (print . fmap sum . sequence . map parseLine . lines) 
