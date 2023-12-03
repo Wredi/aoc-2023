6c3fddedc2cb6cc8eca13143de7b9d639ab40d23
Wredi <piotr.skora25@gmail.com>
2023-12-03 16:56:02 +0100

    day 3 part 2 completed

--- day_03/day_03.hs
+++ day_03/day_03.hs
@@ -28,11 +28,29 @@ safeArrCheck arr check (x,y)
     | otherwise = check (row !! x)
     where row = arr !! y
 
+adjacentCheck :: [[Char]] -> (Char -> Bool) -> IndexedNumber -> Bool
+adjacentCheck table func ((i,j),num) = or [safeArrCheck table func (x,y) | x <- [i-1..i+length num], y <- [j-1..j+1]]
+
 isAdjacentToSymbol :: [[Char]] -> IndexedNumber -> Bool
-isAdjacentToSymbol table ((i,j),num) = or [safeArrCheck table checkFunc (x,y) | x <- [i-1..i+length num], y <- [j-1..j+1]]
+isAdjacentToSymbol table = adjacentCheck table checkFunc
     where checkFunc c = (not $ isDigit c) && (c /= '.')
 
-getPartNumbers :: [[Char]] -> [Int]
+findStarCoords :: [[Char]] -> [Coord]
+findStarCoords table = map fst . filter ((=='*') . snd) . concat $ coordsTable
+    where idxTable = zip [0..] table
+          coordsTable = map (\(y,line) -> map (\(x,c) -> ((x,y),c)) $ zip [0..] line) idxTable 
+
+getPartNumbers :: [[Char]] -> [Int] 
 getPartNumbers arr = (map (read . snd) . filter (isAdjacentToSymbol arr) . getAllNumbers) arr
 
-main = sum . getPartNumbers . lines <$> readFile "input.txt"
+main = do
+    input <- readFile "input.txt"
+    let table = lines input
+    let numbers = filter (adjacentCheck table (=='*')) $ getAllNumbers table
+    let starCoords = findStarCoords table
+    print $ (sum . map (product . map (read . snd)) . filter (\x -> length x == 2) . map (getAdjacentNumbers numbers)) starCoords
+    where getAdjacentNumbers numbers p = filter (isNumberAdjacent p) numbers
+          isNumberAdjacent (xP,yP) ((xN,yN), num) = yN >= yP-1 && yN <= yP+1 
+                                                 && xP >= xN-1 && xP <= xN + length num 
+
+--main = sum . getPartNumbers . lines <$> readFile "input.txt"
