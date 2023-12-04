c8c18f883bcfe39012d08ac1b4d3589d4cf6e341
Wredi <piotr.skora25@gmail.com>
2023-12-04 23:23:44 +0100

    day 4 part 2 completed

--- day_04/day_04.hs
+++ day_04/day_04.hs
@@ -16,8 +16,18 @@ parseCards = map parseCard
 getWinningNumbers :: Card -> [Int]
 getWinningNumbers (w,curr) = intersect w curr 
 
+getCardsCount :: [Card] -> Int -> Int
+getCardsCount cs idx
+    | idx >= length cs = 0
+    | otherwise = len + (sum $ map (getCardsCount cs) $ take len $ [idx+1..])
+    where len = (length . getWinningNumbers) c
+          c = cs !! idx
+
 getPoints :: [Int] -> Int
 getPoints arr = if len == 0 then 0 else 2 ^ (len-1)
     where len = length arr
 
-main = sum . map getPoints . map getWinningNumbers . parseCards . lines <$> readFile "input.txt"
+--main = sum . map getPoints . map getWinningNumbers . parseCards . lines <$> readFile "input.txt"
+main = do
+    cards <- parseCards . lines <$> readFile "input.txt"
+    return (length cards + (sum $ map (getCardsCount cards) $ take (length cards) [0..]))
