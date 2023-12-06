16b3cc31cde6f9dd53ef9f46e48abfd2d4c83d88
Wredi <piotr.skora25@gmail.com>
2023-12-06 21:28:43 +0100

    part 2 day 6 completed

--- src/Day06.hs
+++ src/Day06.hs
@@ -1,6 +1,7 @@
-module Day06 (part1) where
+module Day06 (part1, part2) where
 
 import Data.Bifunctor
+import Data.List
 
 g :: (Floating a, RealFrac a, Integral b) => a -> a -> (b,b)
 g t s = bimap (floor . (+1)) (ceiling . subtract 1) ((t - sqrt (t^2 - 4*s)) / 2, (t + sqrt (t^2 - 4*s)) / 2)
@@ -10,4 +11,10 @@ parseRaces i = case (map (map read) . map (drop 1 . words) . lines) i of
                 [time, recordDist] -> zip time recordDist 
                 _ -> []
 
+parseLongRace :: (Num a, Read a) => String -> (a,a)
+parseLongRace i = case (map (read . intercalate "") . map (drop 1 . words) . lines) i of
+                [time, recordDist] -> (time, recordDist)
+                _ -> (0,0) 
+
 part1 = product . map ((+1) . abs . uncurry (-)) . map (uncurry g) . parseRaces <$> readFile "inputs/day06.txt"
+part2 = (+1) . abs . uncurry (-) . uncurry g . parseLongRace <$> readFile "inputs/day06.txt"
