e5302c4a1fc2d55bcd1e9f00cc366186bddb9eed
Wredi <piotr.skora25@gmail.com>
2023-12-19 11:46:58 +0100

    day 9 part 2 solved

--- src/Day09.hs
+++ src/Day09.hs
@@ -1,12 +1,20 @@
-module Day09 (part1) where
+module Day09 (part1,part2) where
 
 diff :: (Num a) => [a] -> [a]
 diff xs = zipWith (-) (tail xs) xs
 
-nextSeqValue :: [Integer] -> Integer
-nextSeqValue [] = error "not possible"
-nextSeqValue seq
+extrapolateSeq :: ([Integer] -> Integer) 
+               -> (Integer -> Integer -> Integer) 
+               -> [Integer] -> Integer 
+extrapolateSeq element op seq
     | all (==0) seq = 0
-    | otherwise = last seq + nextSeqValue (diff seq)
+    | otherwise = element seq `op` extrapolateSeq element op (diff seq)
+
+nextSeqValue :: [Integer] -> Integer
+nextSeqValue = extrapolateSeq last (+)
+
+prevSeqValue :: [Integer] -> Integer
+prevSeqValue = extrapolateSeq head (-)
 
 part1 = (sum . map nextSeqValue . map (map read . words) . lines) <$> readFile "inputs/day09.txt"
+part2 = (sum . map prevSeqValue . map (map read . words) . lines) <$> readFile "inputs/day09.txt"
